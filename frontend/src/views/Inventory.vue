8ffa2c25a758bc90a1ac334bd0c60e1f5dd35f5d
RyanW-123 <s1348632@live.hkmu.edu.hk>
2025-09-26 16:13:45 +0800

    Update Inventory.vue

--- frontend/src/views/Inventory.vue
+++ frontend/src/views/Inventory.vue
@@ -4,11 +4,10 @@
       <v-col cols="12">
         <v-card>
           <v-card-title>
-            <h1 class="text-h4">Inventory </h1>
+            <h1 class="text-h4">Inventory</h1>
           </v-card-title>
           <v-card-text>
-            <p class="text-h6">this is Inventory page</p>
-            <p>Inventory function</p>
+            <v-data-table :headers = 'headers' :items = 'products'></v-data-table>
           </v-card-text>
         </v-card>
       </v-col>
@@ -17,8 +16,38 @@
 </template>
 
 <script>
+import supabase from '../lib/supabase'
+
+const TABLE_NAME = 'inventory'
+
 export default {
-  name: 'InventoryPage'
+  name: 'InventoryPage',
+  data(){
+    return {
+      headers:[
+        {title: 'ID', value: 'id'},
+        {title: 'Product', value: 'product'},
+        {title: 'Color', value: 'color'},
+        {title: 'Storage', value: 'storage'},
+        {title: 'Stock Quantity', value: 'quantity'},
+        {title: 'Store Name', value: 'name'},
+        {title: 'Store Location', value: 'location'},
+        {title: 'Update Time', value: 'UpdateTime'},
+      ],
+      products: []
+    };
+  },
+  async mounted(){
+    try{
+      const {data, error} = await supabase
+      .from (TABLE_NAME)
+      .select('*');
+      if (error) throw error
+      this.products = data
+    }catch (error){
+      console.error('Error fetching data:', error)
+    }
+  }
 }
 </script>
 
